7c7b48cccb45bc96b466a4489ea778dc093aa5a7
OleksandrBr <bratytsia.o@gmail.com>
2023-01-24 11:59:35 +0200

    addLiksAndUpToSite

--- js/script.js
+++ js/script.js
@@ -16,3 +16,37 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+// page up arrow
+
+// $(document).ready(function(){
+// 	$(window).scroll(function() {
+// 		if ($(this).scrollTop() > 1600) {
+// 			$('.pageup').fadeIn();
+// 		} else $('.pageup').fadeOut();
+// 	});
+// });
+const btnUp = {
+    el: document.querySelector('.btn-pageup'),
+    show() {
+      this.el.classList.remove('btn-pageup_hide');
+    },
+    hide() {
+      this.el.classList.add('btn-pageup_hide');
+    },
+    addEventListener() {
+      window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      document.querySelector('.btn-pageup').onclick = () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
